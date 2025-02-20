ffe893269ec0ffe046d3da4692499b8eb31039e4
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-02-20 14:04:32 +0530

    added environment files

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,3 +1,4 @@
+import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from '../models/models';
@@ -10,8 +11,7 @@ export class LoginService {
   constructor(private readonly http: HttpClient) {  }
 
   login(request: LoginRequest) {
-    return this.http.post<{ token: string, expiresIn: number }>("http://localhost:8081/auth/login", request, {responseType:"json"});
+    console.log(environment.baseUrl);
+    return this.http.post<{ token: string, expiresIn: number }>(`${environment.baseUrl}/auth/login`, request, {responseType:"json"});
   }
-
-
 }
