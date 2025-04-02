4e985f3b8d3346e84f178d9abc42b440443bd5ae
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-04-02 17:35:25 +0530

    add getById method to employee service for fetching employee details by ID

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../environments/environment";
 import {Observable, of} from "rxjs";
+import {EmployeeCreate} from "../models";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,19 @@ export class EmployeeService {
   private readonly baseUrl: string = `${environment.baseUrl}/employee`;
 
   constructor(private readonly http: HttpClient) {
+  }
+
+  // persist
+
+
+  // update
+
+  getById(id: string | null): Observable<EmployeeCreate | null> {
+    if (!id) {
+      return of(null);
+    }
+    const params = new HttpParams().set('id', id);
+    return this.http.get<EmployeeCreate>(`${this.baseUrl}/by-id`, {params});
   }
 
   getNameById(id: string | null): Observable<{ name: string } | null> {
