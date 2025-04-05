fb5ec2d68b98b864b39febcd1474c2e1159703f5
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-04-05 15:15:15 +0530

    refactor UnauthorizedComponent template and remove unused previousUrl property

--- src/app/component/errors/unauthorized/unauthorized.component.ts
+++ src/app/component/errors/unauthorized/unauthorized.component.ts
@@ -14,7 +14,6 @@ import { ToastrService } from "ngx-toastr";
   styleUrls: ['./unauthorized.component.css']
 })
 export class UnauthorizedComponent {
-  previousUrl: string | null = null;
   constructor(
     private readonly location: Location,
     private readonly authService: AuthService,
