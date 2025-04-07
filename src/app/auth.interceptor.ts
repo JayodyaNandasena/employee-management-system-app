12c0c1d71b6452ca431879649c7a3eb512098296
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-04-07 14:41:20 +0530

    refactor: improve formatting and error handling in AuthInterceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,27 +1,49 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {EMPTY, Observable, throwError} from 'rxjs';
+import {catchError, switchMap} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) {
+  }
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = localStorage.getItem('authToken');
 
+    let clonedRequest = request;
     if (token) {
-      const clonedRequest = request.clone({
+      clonedRequest = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
-      return next.handle(clonedRequest);
     }
 
-    return next.handle(request);
+    return next.handle(clonedRequest).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // Handle token expiration
+        if (error.status === 401) {
+          // Redirect to log in
+          this.router.navigate([''], {queryParams: {sessionExpired: true}});
+
+          // Clear invalid token
+          localStorage.removeItem('authToken');
+
+          // Prevent error propagation to .subscribe()
+          return EMPTY;
+        }
+
+        return throwError(() => error);
+      })
+    );
   }
 }
-
